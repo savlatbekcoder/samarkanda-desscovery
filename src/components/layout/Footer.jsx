172445850db3b592f212123d015b327ef2706575
Savlatbek Abdullayev <savlatbek009@gmail.com>
2025-11-09 17:43:26 +0500

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -73,7 +73,7 @@ export default function Footer() {
           </div>
           <div className="child">
             <iframe
-  src="https://www.google.com/maps?q=41.3826929,69.3151987&hl=en&z=15&output=embed"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2994.244630364902!2d69.281747!3d41.3310398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b854a8e73d3%3A0x70b5180b6dab161b!2sDRESS%20UP!%2C%204%20%D1%8D%D1%82%D0%B0%D0%B6%2C%20%D0%B3.%20%D0%A2%D0%B0%D1%88%D0%BA%D0%B5%D0%BD%D1%82%2C%20%D0%9C%D0%B0%D1%81%D1%81%D0%B8%D0%B2%20%D0%9C%D0%B8%D0%BD%D0%BE%D1%80%2C%20%D0%B4%D0%BE%D0%BC%2070%2C%20100017%2C%20Tashkent%2C%20%D0%A3%D0%B7%D0%B1%D0%B5%D0%BA%D0%B8%D1%81%D1%82%D0%B0%D0%BD!5e0!3m2!1sen!2s!4v1731073670000!5m2!1sen!2s"
   width="100%"
   height="400"
   style={{ border: 0 }}
